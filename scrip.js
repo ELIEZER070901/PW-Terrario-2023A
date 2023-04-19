60b83913d66be78ebbab13d7f8017794c6e2602f
Eliezer Reyes <100370498+ELIEZER070901@users.noreply.github.com>
2023-04-19 19:05:01 +0000

    On Pointer Down

--- scrip.js
+++ scrip.js
@@ -1,12 +1,24 @@
-//Tomando control del objeto
+// Haciendo que la planta 1 de mi documento
+// sea arrastable
+dragElement(document.getElementById("plant1"));
 
-let plant1 = document.getElementById("plant1")
-
-//
-plant1.addEventListener("click", function(){
+// Implementando la función drag element
+function dragElement( terrariumElement ){
+    // Creando variables que controlaran las posiciones
+    // iniciales y finales 
+    let pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0
+    // Registrar un evento
+    terrariumElement.onpointerdown = pointerDrag
     
-    let name = prompt("Hola, ¿Cual es tu nombre?");
-    if(name !== ""){
-    console.log(`${name} please, save the planet`);
+    function pointerDrag(event){
+        // Previene todo comportamiento
+        // que tenga por defecto
+        // el evento en cuestion en
+        // mi documento
+        event.preventDefault();
+        console.log("+ On pointer down");
     }
-});
+}
